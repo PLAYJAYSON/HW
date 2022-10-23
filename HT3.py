f02800b1112216679b81c64349c5e6d777f72b3c
PLAYJAYSON <113229924+PLAYJAYSON@users.noreply.github.com>
2022-10-23 19:29:10 +0700

    Add files via upload

---
+++ HT3.py
@@ -0,0 +1,24 @@
+a=int(input('Введите число: '))
+for i in range(1,11):
+    print(a,'x',i,'=', a*i)
+#########################################
+s=0
+a=int(input('Задайте количество чисел: '))
+for i in range(1,a+1):
+    b=float(input('Введите число: '))
+    s+=b
+print('Сумма чисел: ',s)
+##########################################
+a=input('Введите ваш id: ')
+b='id'
+while(a!='PRINT'):
+    print(b+a)
+    a=input('Введите ваш id: ')
+#########################################
+a=float(input('Введите два числа:\n'))
+b=float(input())
+while(b!=0):
+    print(a+b,a-b,a*b,a/b,sep='\n')
+    a = float(input('Введите два числа:\n'))
+    b = float(input())
+print('На ноль делить нельзя!')
