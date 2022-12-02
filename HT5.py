7bdd6325dcc84b30f0099c965bb7622da03b7b86
PLAYJAYSON <113229924+PLAYJAYSON@users.noreply.github.com>
2022-12-02 21:54:57 +0700

    Update HT5.py

--- HT5.py
+++ HT5.py
@@ -12,7 +12,7 @@ print(w.upper())
 w = input('Введите анализируемое слово')
 print("Общее кол-во символов в строке:", len(w))
 print("Первый символ строки:", w[0])
-print("Последний символ строки:", w[-1:])
+print("Последний символ строки:", w[-1])
 print("Строку в обратном порядке:", w[::-1])
 print("Все символы с четными индексами:", w[::2])
 print("Все символы с нечетными индексами:", w[1::2])
@@ -22,20 +22,20 @@ print("Все символы с нечетными индексами:", w[1::2]
 import string
 w = input()
 n = 0
-if w.isdigit():
+if w.isalnum():
     for i in w:
-        print(int(i) * 10,sep=', ')
+        print(int(i) * 10,end=', ')
 elif w.isalpha():
     for i in w:
         if i == 'q' or i == 'Q':
             n += 1
     print(n)
 else:
-    r = 0
-    for i in w:
-        if i in string.punctuation:
-            r += 1
-    print(r)
+
+    for i in range(len(w)):
+        if w[i] in string.punctuation:
+            print(i,end=', ')
+
 
 
 ########################################################
