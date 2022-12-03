b4440aceaa6a833632aea28f2efa0c0eee991bff
PLAYJAYSON <113229924+PLAYJAYSON@users.noreply.github.com>
2022-12-03 11:00:25 +0700

    Update HT5.py

--- HT5.py
+++ HT5.py
@@ -2,14 +2,14 @@
 N = int(input('Введите количество слов:'))
 w = ""
 for i in range(N):
-    print(i+1,'-е слово:',sep='')
+    print(i+1,'-е слово >>>',sep='')
     i = input()
     w += i[0]
 print(w.upper())
 
 
 ###########################################################
-w = input('Введите анализируемое слово')
+w = input('Введите анализируемое слово >>>')
 print("Общее кол-во символов в строке:", len(w))
 print("Первый символ строки:", w[0])
 print("Последний символ строки:", w[-1])
@@ -20,18 +20,17 @@ print("Все символы с нечетными индексами:", w[1::2]
 
 #######################################################
 import string
-w = input()
+w = input('Введите анализируемое слово >>>')
 n = 0
 if w.isalnum():
     for i in w:
         print(int(i) * 10,end=', ')
 elif w.isalpha():
     for i in w:
-        if i == 'q' or i == 'Q':
+        if i.upper() == 'Q':
             n += 1
     print(n)
 else:
-
     for i in range(len(w)):
         if w[i] in string.punctuation:
             print(i,end=', ')
@@ -40,6 +39,7 @@ else:
 
 ########################################################
 while True:
+    n=0
     w = input("Задайте строку: >>>")
     if w.upper() == "STOP":
         break
@@ -48,6 +48,10 @@ while True:
     elif w.isdigit():
         print("Цифровая строка")
     elif w.isascii():
-        print("Знаковая строка")
-    else:
-        print('Смешаная строка')
+        for i in w:
+            if i in string.punctuation:
+                n+=1
+        if n==len(w):
+            print('Знаковая строка')
+        else:
+            print('Смешаная строка')
